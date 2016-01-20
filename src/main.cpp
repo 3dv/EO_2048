031a7b854d61939eecea145b23d66752b62e0084
3dv <edelvalle@openmailbox.org>
2016-01-20 12:11:02 -0430

    SDL2 funciones iniciales

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,65 @@
 /*LLamado al juego!! YUJUU*/
 
 #include <iostream>
-#include "juego.hpp"
-#include <math.h>
+#include "ventana.hpp"
 
 using namespace std;
 
-  int main( int argc, char** args){
-    srand(time(NULL));
-    int dim;
-    cout << "Ingresar dimension de tablero (N x N) : ";
-    cin >> dim;
-    juego J(dim);
-    J.jugar(); 
-     /* while( T.rellenar_casilla() )
-     { T.imprimir_tablero();
-      std::cout << std::endl;}*/
-    /*tabla = new int* [3];
-    for (int i=0 ; i < 3 ; i++)
-      tabla[i] = new int[2];
-    
-    for (int i=0 ; i<3 ; i++){
-      for (int j= 0 ; j<2 ; j++)
-	tabla[i][j]= i+j+2;
-    }
-    
-    std::cout << "dir tabla: " << tabla << std::endl;
-    std::cout << "dir *tabla: " << *tabla << std::endl;
-    std::cout << "dir tabla[0]: " << tabla[0] << std::endl;
-    std::cout << "dir tabla[1]: " << tabla[1] << std::endl;
-    std::cout << "dir *tabla[0]: " << *tabla[0] << std::endl;
-    std::cout << "dir *tabla[1]: " << *tabla[1] << std::endl;
-    std::cout << "dir **tabla: " << tabla[2][1] << std::endl;
-    
-    for (int j=0 ; j<2 ; j++){
-      for (int i= 0 ; i<3 ; i++)
-	std::cout<< "\t" << tabla[i][j];// << "\t";
-      std::cout<<std::endl;
-    }
-    for ( int i=0 ; i <3; i++)
-      delete[] tabla[i];
-    delete[] tabla;*/
-   /* srand(time(NULL));
-    for(int i=0; i<3;i++){
-    int a = round(rand()%3 + 2);
-   std::cout << a << std::endl;}*/
-    return 0;
-  } 
+int main( int argc, char** args){
+	//juego* J = new juego();
+	//J->jugar();
+
+	bool salir = false;
+
+	ventana* vent = new ventana();
+
+	SDL_Event evento;
+
+	direccionJugada dir = NINGUNA;
+
+	while ( !salir ) {
+		while( SDL_PollEvent( &evento ) != 0 ) {
+			if ( evento.type == SDL_QUIT ) {
+				salir = true;
+				dir = SALIR;
+			}else if( evento.type == SDL_KEYDOWN ) {
+				vent->iniciar_juego(4);
+				switch( evento.key.keysym.sym )
+				{
+				case SDLK_UP:
+					dir = ARRIBA;
+					break;
+
+				case SDLK_DOWN:
+					dir = ABAJO;
+					break;
+
+				case SDLK_LEFT:
+					dir = IZQUIERDA;
+					break;
+
+				case SDLK_RIGHT:
+					dir = DERECHA;
+					break;
+
+				default:
+					break;
+				}
+			}
+		}
+		vent->dibujar(dir);
+		dir = NINGUNA;
+		/*if( tabla->jugada_disponible() ) {
+		        pedir_jugada();
+		        ejecutar_jugada();
+		   }else{
+		        finalizar_juego();
+		   }*/
+
+
+	}
+	delete vent;
+
+
+	return 0;
+}
